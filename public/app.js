04b7ff22cc4601f8491e078d507460fb2706025e
Tolby Hunt <hunt_tolby@yahoo.com>
2017-06-27 14:59:25 -0600

    New branch with updated styling

--- public/app.js
+++ public/app.js
@@ -1,4 +1,4 @@
-var app = angular.module("JabberTalkee", ["ngRoute", "Auth"]);
+var app = angular.module("JabberTalkee", ["ngRoute", "Auth","ngToast"]);
 
 app.config(["$routeProvider", function ($routeProvider) {
 
