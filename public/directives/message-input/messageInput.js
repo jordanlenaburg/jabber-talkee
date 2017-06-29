cbc6571c026f8e2c2d7f3a8c839f2f4674acfccb
Jordan Lenaburg <jordan.lenaburg@gmail.com>
2017-06-29 10:30:17 -0600

    minor fix with channel add users

--- public/directives/message-input/messageInput.js
+++ public/directives/message-input/messageInput.js
@@ -2,7 +2,7 @@ angular.module("JabberTalkee")
     .controller("messageCtrl", ["$scope", "$routeParams", "messageService", function ($scope, $routeParams, messageService) {
 
         $scope.sendMessage = function (message) {
-            console.log($routeParams);
+            // console.log($routeParams);
             message.channel_id = $routeParams.id;
             // console.log(message)
             messageService.sendMessage(message)
